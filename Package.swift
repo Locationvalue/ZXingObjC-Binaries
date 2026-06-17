9eb475f93dbc758f62d1becab0b5c6ae4ca34b73
masunomoeka <masuno@dearone.io>
2026-06-17 14:13:20 +0900

    fix: checksum

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZXingObjC-Binaries",
-            url: "https://github.com/Locationvalue/ZXingObjC-Binaries/releases/download/1.0.0/ZXingObjC-Binaries.xcframework.zip",
-            checksum: "1b84e1a5801cce19b810df091ef11a44969e8a852932c8c88d9c4e176d539add"
+            url: "https://github.com/Locationvalue/ZXingObjC-Binaries/releases/download/1.0.0/ZXingObjCBinary.xcframework.zip",
+            checksum: "43c1d35cd88de2fcf1f6b6b4405c3ff40d269f99ca509a6b2b03b660b9d3e6d2"
         )
     ]
 )
